a39bbaa418d3d52bbcba2dde2b3776f1d43f5b1d
nicolas <nico_kol@hotmail.com>
2025-07-09 23:41:46 -0300

    Updates

--- assets/FomatoPrecio-B_Wz0Nci.js
+++
@@ -1,1 +0,0 @@
-const r=i=>Number(i).toLocaleString("es-ES",{minimumFractionDigits:2,maximumFractionDigits:2});export{r as f};
---
+++ assets/FomatoPrecio-C9Oljihr.js
@@ -0,0 +1,1 @@
+function r(t,i=!0){return t.toLocaleString("es-AR",{minimumFractionDigits:i?2:0,maximumFractionDigits:i?2:0})}export{r as f};
