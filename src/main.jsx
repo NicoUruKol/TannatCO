a054e59d518faf504b2d52b03a77f5610d39f94e
Nicolas <nico_kol@hotmail.com>
2025-11-14 14:34:19 -0300

    sigue adaptacion para vercel

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename={import.meta.env.PROD ? "/TannatCO" : "/"}>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <AuthProvider>        
       <AdminProvider>     
         <CarritoProvider>
